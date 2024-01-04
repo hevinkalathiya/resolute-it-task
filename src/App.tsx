a0b54143f4341fcc67e3ad7919152dbf08f784c8
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-01-04 18:28:12 +0530

    Fix: build Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
-=======
 import "./App.css";
 import Description from "./components/Description";
 import Hero from "./components/Hero";
@@ -22,7 +12,6 @@ function App() {
       <Description />
     </div>
   );
->>>>>>> 03d48ce (Feat: Navbar Created)
 }
 
 export default App;

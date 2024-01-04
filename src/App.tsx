0f3f6125a036df51c15cc5a4074b41bf1da9f65f
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-01-04 16:21:47 +0530

    Feat: Navbar Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -7,6 +8,19 @@ function App() {
   const [count, setCount] = useState(0);
 
   return <></>;
+=======
+import "./App.css";
+import Hero from "./components/Hero";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="max-w-[1500px] mx-auto">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+>>>>>>> 03d48ce (Feat: Navbar Created)
 }
 
 export default App;

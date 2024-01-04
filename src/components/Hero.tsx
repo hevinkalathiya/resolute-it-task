243ecb2fb9e69a858d4b8494b86bb5e904c87abe
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-01-04 17:34:44 +0530

    Feat: Hero Section Created

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div>
+      <div className=" bg-image h-[300px] md:h-[500px] ">
+        <div className=" mt-3 flex items-center justify-center flex-col">
+          <h4 className="text-[20px] font-medium text-gray-800">
+            Simple, Powerful &amp; Affordable
+          </h4>
+          <h2 className="text-[#002365] text-[25px] font-extrabold">
+            POS SOFTWARE SOLUTION
+          </h2>
+          <h4 className="text-[20px] font-medium text-gray-800">
+            For All Restaurant Types
+          </h4>
+          <button className="inline-flex items-center justify-center rounded-md p-2 bg-[#002365] text-white text-sm  hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black mt-2">
+            Book A Demo
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
